3be96c803dce464ea9011b7a27d401c090fffab3
HyperionPanda <aidanghorn1@gmail.com>
2024-05-02 12:40:46 -0700

    added randomize function to box shuffle

--- RandomWeb/src/app/components/box-shuffle/box-shuffle.component.ts
+++ RandomWeb/src/app/components/box-shuffle/box-shuffle.component.ts
@@ -13,9 +13,34 @@ export class BoxShuffleComponent {
 
   @Input() basicList: String[];
   timeHandler: TimeHandlerService = new TimeHandlerService();
+  previousWinner: number = 21;
+  currentWinner: String = "";
 
   constructor(private el : ElementRef, private renderer : Renderer2){this.basicList = [];}
+  
+  randomPick(){
 
+    let len = this.basicList.length;
+
+    while(true){
+      const val = Math.floor(Math.random() * (len));
+
+      if (this.previousWinner == 21){
+        this.previousWinner = val;
+        return val;
+      }else{
+
+        if(this.previousWinner != val){
+          console.log("previous = "+this.previousWinner)
+          console.log("current = "+val)
+          this.previousWinner = val;
+          return val;
+        }
+      }
+      return val;
+    }
+  }
+    
 
   async shuffle(){
     const wholeBox = this.el.nativeElement.querySelector(".container");
@@ -41,14 +66,9 @@ export class BoxShuffleComponent {
       
     }
     this.renderer.setStyle(wholeBox,"transform","translate(0px)");
-    //this.renderer.setStyle(wholeBox,"transform","rotate()");
 
-
-    //this.renderer.setStyle(wholeBox,"bottom","100px");
+    this.currentWinner = this.basicList[this.randomPick()];
     
-    //this.renderer.setStyle(wholeBox,"transform","translate(-40px,-25px)");
-
-
-
+    
   }
 }
