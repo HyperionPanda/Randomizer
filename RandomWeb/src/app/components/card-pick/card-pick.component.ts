ff71324089a550f94627cc91f8b57c856d24a078
HyperionPanda <aidanghorn1@gmail.com>
2024-05-27 21:39:12 -0700

    can select and deselect cards as well as keep track of them

--- RandomWeb/src/app/components/card-pick/card-pick.component.ts
+++ RandomWeb/src/app/components/card-pick/card-pick.component.ts
@@ -13,10 +13,22 @@ export class CardPickComponent {
 
   @Input() basicList: String[];
 
-  constructor(private el : ElementRef, private renderer : Renderer2){this.basicList = [];}
+  selectedList : String[];
 
-  selectCard(){
-    alert();
+  constructor(private el : ElementRef, private renderer : Renderer2){this.basicList = []; this.selectedList = [];}
+
+  selectCard(id : Number){
+  
+    const element = this.el.nativeElement.querySelector("#b"+id);
+    
+    if(this.selectedList.includes(element)){
+      this.renderer.setStyle(element,"border","none");
+      this.selectedList.splice(this.selectedList.indexOf(element),1);
+    }else{
+      this.renderer.setStyle(element,"border","10px solid yellow");
+      this.selectedList.push(element);
+    }
+
   }
 
 }
