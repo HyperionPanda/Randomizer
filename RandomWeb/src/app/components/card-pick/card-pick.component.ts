946261532722fdd501870beb432e9de9996c54f2
HyperionPanda <aidanghorn1@gmail.com>
2024-06-01 21:54:37 -0700

    fixed card selection and now unselects after it removes unselected cards

--- RandomWeb/src/app/components/card-pick/card-pick.component.ts
+++ RandomWeb/src/app/components/card-pick/card-pick.component.ts
@@ -16,6 +16,8 @@ export class CardPickComponent implements OnInit{
 
   selectAmount : number = 1;
 
+  lastCard : String = "";
+
   constructor(private el : ElementRef, private renderer : Renderer2, private timeHandler: TimeHandlerService){this.basicList = []; this.selectedList = [];}
 
   ngOnInit(): void {
@@ -23,19 +25,35 @@ export class CardPickComponent implements OnInit{
   }
 
   determineSelectionAmount(): void {
-      const length = this.basicList.length;
 
-      if(length != 5 && length % 5 == 1){
-        this.selectAmount = 5;
-      }else if (length != 3 && length % 3 == 1){
-        this.selectAmount = 3;
-      }else if (length != 2 && length % 2 == 1){
-        this.selectAmount = 2;
+    if(this.selectedList.length == 1){
+      this.lastCard = this.selectedList[0];
+    }
+
+    if (this.selectedList.length <= 0 && this.basicList.length > 1){
+        this.selectAmount = this.basicList.length-1;
+    }else if(this.basicList.length == 1){
+        this.selectAmount = 1;
+    }else{
+      if(this.selectedList.length != 5 && this.selectedList.length % 5 == 1){
+          this.selectAmount = 5;
+      }else if (this.selectedList.length != 3 && this.selectedList.length % 3 == 1){
+          this.selectAmount = 3;
+      }else if (this.selectedList.length != 2 && this.selectedList.length % 2 == 1){
+          this.selectAmount = 2;
+      }else{
+          this.selectAmount = 1;
       }
-      console.log(this.selectAmount);
+    }
+      
   }
 
   selectCard(id : String){
+
+    if(this.selectAmount == 1){
+      this.determineSelectionAmount();
+    }
+
       const element = this.el.nativeElement.querySelector("#b-"+id);
       
       //if card selected, unselect it. Else if card unselected, select it
@@ -48,37 +66,47 @@ export class CardPickComponent implements OnInit{
         if(this.selectedList.length < this.selectAmount){
         this.renderer.setAttribute(element,"class","card selected")
         this.selectedList.push(element.id);
-        }else{
-        console.log("Cannot select any more cards");
         }
       }
     
   }
 
   async finalSelection(){
-
-  
+    // get rid of all cards that are unselected
     for(let i = 0; i < this.basicList.length; i++){
+
       const element = this.el.nativeElement.querySelector("#b-"+this.basicList[i]);
+
       if(this.selectedList.includes("b-"+this.basicList[i])){
         console.log(this.basicList[i]+" stays");
-        //this.renderer.setAttribute(element,"class","card unselected");
       }else{
+
         this.renderer.setAttribute(element,"class","card unselected removeUnselected");
         await this.timeHandler.delay(2);
         this.renderer.setStyle(element, "display","none");
       }
       
     }
-    if(this.selectedList.length <= 1){
+
+    this.determineSelectionAmount();
+
+    //unselect all selected cards after card removal
+    for(let j = 0; j < this.selectedList.length; j++){
+      const selectedElement = this.el.nativeElement.querySelector("#"+this.selectedList[j]);
+
+      this.renderer.setAttribute(selectedElement,"class","card unselected")
+    }
+    this.selectedList = [];
+    
+    if(this.lastCard != ""){
       this.flip();
     }
-    this.determineSelectionAmount();
   }
+  //flips last remaining card
   flip(){
-    const element = this.el.nativeElement.querySelector("#"+this.selectedList[0]);
-    const cardValue = this.el.nativeElement.querySelector("#v-"+this.selectedList[0]);
-    console.log("#v-"+this.selectedList[0]);
+    const element = this.el.nativeElement.querySelector("#"+this.lastCard);
+    const cardValue = this.el.nativeElement.querySelector("#v-"+this.lastCard);
+  
     this.renderer.setAttribute(element,"class","flip");
     this.renderer.removeAttribute(cardValue,"hidden");
   }
